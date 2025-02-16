321f3deb0b4daa21bcdecc070c5d1cc0fe8327b7
Thomas Chappell (UVU Compu Spring 2025) <jthomaschappell@gmail.com>
2025-02-16 05:27:25 -0700

    significant progress on home. We have the top bar and the call to action and the main image underneath

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,14 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 // import Logo from './assets/binder_better.svg'
 import { Button, Card, Form, Container } from "react-bootstrap";
+import { useNavigate } from 'react-router-dom';
 import Logo from './assets/logo.svg';
+import MainImage from './assets/grandma_reading.jpg';
 
 function Navbar() {
   return (
     <nav className='navbar'>
       <ul>
-        {/* <li><Link to="/"> */}
-        {/* <img src={Logo} alt="Logo" width="128"/></Link></li> */}
-        {/* <img src={Logo} alt="Logo" width="32" height="32" />
- */}
         <li><Link to="/gallery">Gallery</Link></li>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/login">Login</Link></li>
@@ -24,27 +22,35 @@ function Navbar() {
 function Home() {
   return (
     <>
-      <h1 className="greeting">Hi Everyone</h1>
-      <Button onClick={() => alert("Hello!")}>Say Hello</Button>
+      <h1 className="display-4 fw-bold mb-4">Custom Book Covers,<br></br> Tailored to Your Story</h1>
+      <p className="lead text-muted mb-4 fw-semibold">Transform your books with perfectly fitted, beautifully designed covers
+      </p>
+      <Button className="mb-4" onClick={() => alert("You just scheduled a demo!")}>LEARN MORE</Button>
+
+      {/* We need a Call to Action button here */}
+      <div className="d-flex justify-content-center mb-4">
+        <img src={MainImage} className="img-fluid w-100 rounded-4" alt="description" />
+      </div>
     </>
   );
 }
 
 function Login() {
+  const navigate = useNavigate(); 
   function handleSubmit(event) {
     event.preventDefault();
-    const form = event.target;
-    alert(`Name: ${form.email.value}\n` +
-      `Email: ${form.password.value}\n`);
+    // const form = event.target;
+    // TODO: Test this. 
+    alert(`Login successful!`);
+    navigate("/"); 
   }
-
   return (
     <Container className="mt-4 d-flex justify-content-center">
 
       {/* <h2 className="mb-3">Login</h2> */}
-      <Card style={{ width: '400px'}}>
-      <Card.Body>
-      <div className="text-center mt-4 mb-4">
+      <Card style={{ width: '400px' }}>
+        <Card.Body>
+          <div className="text-center mt-4 mb-4">
             <img src={Logo} alt="Binder Logo" style={{ width: "250px", height: 'auto' }} />
           </div>
           <Card.Title as="h1" className="mb-4 text-center">Login</Card.Title>
